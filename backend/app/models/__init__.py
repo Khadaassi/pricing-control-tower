6d982584741e9ab54319368e16dca56094ccaa20
Khadaassi <aassikhadija@gmail.com>
2026-04-10 12:49:48 +0200

    feat(models): add Promotion model and update __all__ in models init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.base import Base
 from app.models.price import Price
 from app.models.product import Product
 from app.models.product_family import ProductFamily
+from app.models.promotion import Promotion
 
-__all__ = ["Base", "Product", "ProductFamily", "Price"]
+__all__ = ["Base", "Product", "ProductFamily", "Price", "Promotion"]
