a74a02a166009eb34e7565f22ea38a07de4f9874
Khadaassi <aassikhadija@gmail.com>
2026-05-05 11:03:11 +0200

    feat(kpis): implement sales KPIs endpoint and related models

--- backend/alembic/versions/fec186f3ed43_update_unit_price_constraint_to_be_.py
+++ backend/alembic/versions/fec186f3ed43_update_unit_price_constraint_to_be_.py
@@ -8,8 +8,6 @@ Create Date: 2026-04-19 11:58:55.379188
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = 'fec186f3ed43'
--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
+from .routes.kpis import router as kpis_router
 from .routes.prices import router as prices_router
 from .routes.products import router as products_router
 from .routes.promotions import router as promotions_router
-from .routes.technical import router as technical_router
 from .routes.sales import router as sales_router
+from .routes.technical import router as technical_router
 
 api_router = APIRouter()
 
@@ -13,3 +14,4 @@ api_router.include_router(products_router)
 api_router.include_router(prices_router)
 api_router.include_router(promotions_router)
 api_router.include_router(sales_router)
+api_router.include_router(kpis_router)
